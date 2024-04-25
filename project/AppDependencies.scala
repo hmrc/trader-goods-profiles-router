6b6c40f96e9a979e682b45ee858aeb4e6633f824
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2024-04-25 11:37:36 +0100

    TGP-529: Add mockito-scala dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,14 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "8.4.0"
-  
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion
+    "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
-    
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
+    "org.mockito" %% "mockito-scala" % "1.17.31" % Test
   )
 
   val it = Seq.empty
