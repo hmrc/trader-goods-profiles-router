62d38f0372893451c03124a568d4efe306ce9c8f
IoannisLazaridis-oc <128503072+IoannisLazaridis-oc@users.noreply.github.com>
2024-04-29 21:48:03 +0100

    TGP-529: Added EISInstanceConfig
    - Updated application.conf including bearer-token under headers
    - Updated AppConfig loading the configuration for EIS service
    - Updated EisConnectorImpl using the `instanceConfig values`
    - Updated existing test EISConnectorSpec mocking the `eisConfig`

--- app/uk/gov/hmrc/tradergoodsprofilesrouter/config/AppConfig.scala
+++ app/uk/gov/hmrc/tradergoodsprofilesrouter/config/AppConfig.scala
@@ -16,11 +16,14 @@
 
 package uk.gov.hmrc.tradergoodsprofilesrouter.config
 
-import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 
-@Singleton
-class AppConfig @Inject()(config: Configuration) {
+import javax.inject.{Inject, Singleton}
 
-  val appName: String = config.get[String]("appName")
+@Singleton
+class AppConfig @Inject() (config: Configuration) {
+
+  val appName: String              = config.get[String]("appName")
+  val eisConfig: EISInstanceConfig = config.get[EISInstanceConfig]("microservice.services.eis")
 }
+
