73a1bffebf658763c2125c3067be865a5fc7725d
Amar Jagtap <amar.jagtap@digital.hmrc.gov.uk>
2024-04-30 16:55:25 +0100

    TGP-529: Added EIS response models

---
+++ app/uk/gov/hmrc/tradergoodsprofilesrouter/models/eis/response/GoodsItemRecords.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.tradergoodsprofilesrouter.models.eis.response
+
+case class GoodsItemRecords(
+  eori: String,
+  actorId: String,
+  recordId: String,
+  traderRef: String,
+  comcode: String,
+  accreditationRequest: String,
+  goodsDescription: String,
+  countryOfOrigin: String,
+  category: Int,
+  assessments: Option[Seq[Assessment]],
+  supplementaryUnit: Option[Int],
+  measurementUnit: Option[String],
+  comcodeEffectiveFromDate: String,
+  comcodeEffectiveToDate: Option[String],
+  version: Int,
+  active: Boolean,
+  toReview: Boolean,
+  reviewReason: Option[String],
+  declarable: String,
+  ukimsNumber: String,
+  nirmsNumber: Option[String],
+  niphlNumber: Option[String],
+  locked: Boolean,
+  srcSystemName: String,
+  createdDateTime: String,
+  updatedDateTime: String
+)
