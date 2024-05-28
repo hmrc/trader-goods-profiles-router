de5c60f2c80d501c3e82a05970535546a528b351
paul.olabisi <162993468+pauloncast@users.noreply.github.com>
2024-05-28 08:13:25 +0100

    [TGP-423] Implement Update Record endpoint

--- test/uk/gov/hmrc/tradergoodsprofilesrouter/utils/ValidationSupportSpec.scala
+++ test/uk/gov/hmrc/tradergoodsprofilesrouter/utils/ValidationSupportSpec.scala
@@ -17,7 +17,9 @@
 package uk.gov.hmrc.tradergoodsprofilesrouter.utils
 
 import org.scalatestplus.play.PlaySpec
-import uk.gov.hmrc.tradergoodsprofilesrouter.utils.ValidationSupport.{isValidCountryCode, isValidDate}
+import play.api.libs.json.{JsPath, JsonValidationError}
+import uk.gov.hmrc.tradergoodsprofilesrouter.models.response.errors.Error
+import uk.gov.hmrc.tradergoodsprofilesrouter.utils.ValidationSupport.{convertError, isValidCountryCode, isValidDate}
 
 class ValidationSupportSpec extends PlaySpec {
 
@@ -33,6 +35,55 @@ class ValidationSupportSpec extends PlaySpec {
     }
     "an invalid countryCode must return false" in {
       isValidCountryCode("GB098765112") mustBe false
+    }
+
+    "convertError" should {
+      "for actorId" in {
+        val error = JsonValidationError("error.path.missing")
+        val path  = JsPath \ "actorId"
+
+        val errors         = scala.collection.Seq(
+          (path, scala.collection.Seq(error))
+        )
+        val expectedErrors = Seq(
+          Error(
+            "INVALID_REQUEST_PARAMETER",
+            "Mandatory field actorId was missing from body or is in the wrong format",
+            8
+          )
+        )
+
+        val result = convertError(errors)
+
+        result mustBe expectedErrors
+      }
+
+      "for traderRef and comcode" in {
+        val error = JsonValidationError("error.path.missing")
+        val path1 = JsPath \ "traderRef"
+        val path2 = JsPath \ "comcode"
+
+        val errors         = scala.collection.Seq(
+          (path1, scala.collection.Seq(error)),
+          (path2, scala.collection.Seq(error))
+        )
+        val expectedErrors = Seq(
+          Error(
+            "INVALID_REQUEST_PARAMETER",
+            "Mandatory field traderRef was missing from body or is in the wrong format",
+            9
+          ),
+          Error(
+            "INVALID_REQUEST_PARAMETER",
+            "Mandatory field comcode was missing from body or is in the wrong format",
+            11
+          )
+        )
+
+        val result = convertError(errors)
+
+        result mustBe expectedErrors
+      }
     }
   }
 }
