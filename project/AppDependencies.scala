d717b191e0bee78ddffb098c34a1043d526d27e4
nonebula <147166151+nonebula@users.noreply.github.com>
2025-06-24 12:15:48 +0100

    TGP-3479 - address pr-bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  private val catsVersion      = "2.6.1"
+  private val catsVersion      = "2.13.0"
   private val bootstrapVersion = "9.8.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-backend-play-30" % bootstrapVersion,
     "org.typelevel"    %% "cats-core"                 % catsVersion,
     "commons-validator" % "commons-validator"         % "1.9.0",
-    "com.beachape"     %% "enumeratum-play"           % "1.8.0"
+    "com.beachape"     %% "enumeratum-play"           % "1.9.0"
   )
 
   val test = Seq(
