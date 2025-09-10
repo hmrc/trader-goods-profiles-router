1f77e9d8287cb66d93a24b070b3a7f30be7f0ed5
Ben Healy <139165148+taylors-version@users.noreply.github.com>
2025-09-10 09:12:08 +0100

    TGP-3571 Update dependencies (#154)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   private val catsVersion      = "2.13.0"
-  private val bootstrapVersion = "9.16.0"
+  private val bootstrapVersion = "9.19.0"
 
   val compile = Seq(
     "uk.gov.hmrc"      %% "bootstrap-backend-play-30" % bootstrapVersion,
@@ -13,8 +13,8 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"   % bootstrapVersion % Test,
-    "org.scalatestplus" %% "mockito-4-11"            % "3.2.18.0"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion % Test,
+    "org.scalatestplus" %% "mockito-4-11"           % "3.2.18.0"
   )
 
   val it = Seq.empty
